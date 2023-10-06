0a9eb0cba23b508f20b0bc8c5349bf122bfdc82d
Mattymatics <iyogunmathias@gmail.com>
2023-10-06 16:55:10 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints saize of datatypes
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byts(s)\n", sizeof(long));
+	printf("Size of a long long int: %D bytes(s)\n", sizeof(long long));
+	printf("SIze of a float int: %d byte(s)\n", sizeof float));
+	return (0);
+}
