340df1a8486af6c359551753f32702302c782217
Mattymatics <iyogunmathias@gmail.com>
2023-10-08 09:12:42 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets in lowercase and uppercase
+ * Return - Always 0
+ */
+int main(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+		putchar(letters);
+
+	for (letters = 'A'; letters <= 'Z'; letters++)
+		putchar(letters);
+
+	putchar('\n');
+
+	return (0);
+}
