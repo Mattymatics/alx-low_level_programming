6c70d8ecad3171510294d81d59f60353bd293f7c
Mattymatics <iyogunmathias@gmail.com>
2023-10-08 09:25:00 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <sstdio.h>
+
+/**
+ * main - prints the alphabets in lowercase except q and e
+ * Return - Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			continue;
+		}
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
