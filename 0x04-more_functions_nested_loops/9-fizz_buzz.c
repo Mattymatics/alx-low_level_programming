d4742300a129882d541b0ca1c24a2ead5c4c6087
Mattymatics <iyogunmathias@gmail.com>
2023-10-15 07:28:53 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * Fizz_buzz - prints fizz for multiples of 3, and buzz for 5
+ *
+ * Return: Always 0
+ */
+
+int Fizz_buzz(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			_putchar("FizzBuzz ");
+		}
+		else if ( i % 3 == 0)
+		{
+			_putchar("Fizz ");
+		}
+		else if ( i % 5 == 0)
+		{
+			_putchar("Buzz ");
+		}
+		else
+		{
+			_putchar('0' + i);
+		}
+	}
+}
+
+
