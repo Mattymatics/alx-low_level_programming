bab8fbd6ef3f4a5bb9d13e5c61c0c6caab869779
Mattymatics <iyogunmathias@gmail.com>
2023-10-16 13:55:14 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - the functions returns the string length
+ * @s: string
+ * Return: len
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
