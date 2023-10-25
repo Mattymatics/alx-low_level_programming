f43134da13f3848162eafcaf56e64862cce9805e
Mattymatics <iyogunmathias@gmail.com>
2023-10-25 14:22:24 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - a funxtion that returns the value of X^y
+ * @x: value
+ * @y: power
+ * Return: X^y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	
+	return(x * _pow_recursion(x, y - 1);
+}
