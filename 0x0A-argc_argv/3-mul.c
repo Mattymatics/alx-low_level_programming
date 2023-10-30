c2872dcc752a0b5c3662375c89205190a4e86e62
Mattymatics <iyogunmathias@gmail.com>
2023-10-30 15:40:39 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,14 +11,16 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argc;
+	int a, b;
 
-	if (argv[1] && argv[2])
+	if (argc < 3)
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-		return (0);
-	}
-	else
 		printf("Error\n");
-	return (1);
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d\n", a * b);
+
+	return (0);
 }
