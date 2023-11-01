44938b77432275260a273a50a22681210c728789
Mattymatics <iyogunmathias@gmail.com>
2023-11-01 16:02:42 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+include "main.h"
+
+/**
+ * free_grid - funtion frees a 2D created by alloc_grid
+ * @grid: param - alloc_grid
+ * @height: height of grid
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height; a++)
+	{
+		free(grid[a]);
+	}
+	free(grid);
+}
