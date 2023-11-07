2f4978d764aab9f68915c9489ac1036705153c52
Mattymatics <iyogunmathias@gmail.com>
2023-11-07 14:06:44 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,14 +7,15 @@
  * @name: string
  * @age: integer string
  * @owner: string - owner
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	if (d == 0)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
