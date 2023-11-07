ffe35da77b5ef4dca216f6532f0c1621f01a1a36
Mattymatics <iyogunmathias@gmail.com>
2023-11-07 14:00:43 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,7 @@
 
 /**
  * struct dog - definition
+ * init_dog - definition
  * @name: string
  * @age: integer
  * @owner: string
@@ -15,14 +16,11 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
-void print_dog(struct dog *d);
 typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
-
 
 #endif
