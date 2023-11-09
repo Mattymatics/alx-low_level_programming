094be640cb31b697dde3dbfa2d9009afe083c341
Mattymatics <iyogunmathias@gmail.com>
2023-11-09 13:23:45 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that ptints a name
+ * @name: name to print
+ * @f: pointer to the print function
+ */
+
+void print_name(char *name, void (*f)(char *))
+
+{
+	if (!name || !f)
+
+		return;
+
+	f(name);
+}
